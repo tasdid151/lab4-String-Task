8e520b988cfc70272dd9eda723143df6e3e8ebe8
tasdid151 <109462135+tasdid151@users.noreply.github.com>
2022-08-10 09:03:40 +0600

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main()
+{
+    int i,l;
+    string s;
+    cin>>s;
+    for(i=0; i<s.size(); i++)
+    {
+        char c=tolower(s[i]);
+        if(c!='a'&&c!='e'&&c!='i'&&c!='o'&&c!='u'&&c!='y')
+          {
+              cout<<"."<<c;
+          }
+    }
+
+}
